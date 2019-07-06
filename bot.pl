ef4691528dad3effd08b8def08248c71d3675b0f
Siren <siren@kernal.eu>
2019-07-07 02:41:34 +0300

    Get updates and echo for test

--- bot.pl
+++ bot.pl
@@ -11,4 +11,8 @@ my $bot = load_config("config.json");
 my $bot_token = $bot->{bot_token};
 my $chat_id = $bot->{chat_id};
 
-send_message($bot_token, $chat_id, "t.me/kernals");
+# send_message($bot_token, $chat_id, "perl");
+
+while (1) {
+    get_updates($bot_token, $chat_id);
+}
--- tg.pl
+++ tg.pl
@@ -1,7 +1,8 @@
 use strict;
 use warnings;
 
-use LWP::UserAgent;
+use LWP::UserAgent qw( );
+use LWP::Simple;
 
 my $base_url = "https://api.telegram.org/";
 
@@ -9,9 +10,27 @@ sub send_message
 {
     my ($token, $chat, $message) = @_;
     my $method = "sendMessage";
-    my $url = $base_url."bot".$token."/".$method;
+    my $url = "${base_url}bot${token}/${method}";
     my $ua = LWP::UserAgent->new();
     my $response = $ua->post($url, [chat_id => $chat, text => $message]);
+}
+
+my $offset = 0;
+
+sub get_updates
+{
+    my ($token, $chat) = @_;
+    my $method = "getUpdates";
+    my $url = "${base_url}bot${token}/${method}?offset=${offset}";
+    my $updates = get($url);
+    my $json = decode_json($updates);
+    
+    for my $update (@{$json->{result}}) {
+        if ($update->{message}->{chat}->{id} == $chat) {
+            send_message($token, $chat, $update->{message}->{text});
+            $offset = $update->{update_id}+1;
+        }
+    }
 }
 
 1;
